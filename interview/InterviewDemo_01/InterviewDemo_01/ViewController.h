39714706ee87e336f25ebb2207ad71fd2cc4c27d
qi <qizhang@tvunetworks.com>
2018-05-04 19:11:13 +0800

    update

---
+++ interview/InterviewDemo_01/InterviewDemo_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterviewDemo_01
+//
+//  Created by qi on 2018/5/4.
+//  Copyright © 2018 tvu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ interview/InterviewDemo_01/InterviewDemo_01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InterviewDemo_01
+//
+//  Created by qi on 2018/5/4.
+//  Copyright © 2018 tvu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
